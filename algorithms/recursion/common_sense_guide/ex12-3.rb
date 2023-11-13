0fac67795c0dd38355fa54d8822fd38b4bd2c2d6
tsangsiu <tsangsiu@gmail.com>
2023-11-13 09:49:49 +0000

    Minor amendment to ex12-3.rb

--- algorithms/recursion/common_sense_guide/ex12-3.rb
+++ algorithms/recursion/common_sense_guide/ex12-3.rb
@@ -7,8 +7,8 @@ end
 def number_of_shortest_paths(n_row, n_col, memo = {})
   return 1 if n_row == 1 || n_col == 1
   return memo[[n_row, n_col]] unless memo[[n_row, n_col]].nil?
-  memo[[n_row, n_col]] = number_of_shortest_paths(n_row - 1, n_col) +
-                          number_of_shortest_paths(n_row, n_col - 1)
+  memo[[n_row, n_col]] = number_of_shortest_paths(n_row - 1, n_col, memo) +
+                          number_of_shortest_paths(n_row, n_col - 1, memo)
 end
 
 puts number_of_shortest_paths(7, 3)
